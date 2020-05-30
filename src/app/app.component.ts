50ad3bce0564f67f8316584722b8740cd9415f86
Josue Castro <josue.castro@upr.edu>
2020-05-29 21:35:50 -0400

    Main page view working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  layout = 1;
+  constructor() {}
+  ngOnInit(): void {
+    const setLayout = setInterval(() => {
+      this.layout++;
+      if (this.layout == 4) {
+        clearInterval(setLayout);
+      }
+    }, 30 * 1000);
+  }
+}
